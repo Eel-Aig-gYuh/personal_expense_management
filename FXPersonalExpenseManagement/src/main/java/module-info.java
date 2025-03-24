483cd9ccf2dc3bfcf6cb3786d2f0da1e1365bbfd
Hue <hoanghue26804@gmail.com>
2025-03-24 22:18:17 +0700

    Delete FXPersonalExpenseManagement directory

--- FXPersonalExpenseManagement/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.nhom4.fxpersonalexpensemanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.nhom4.fxpersonalexpensemanagement to javafx.fxml;
-    exports com.nhom4.fxpersonalexpensemanagement;
-}
