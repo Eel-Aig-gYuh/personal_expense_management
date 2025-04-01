4a28e6c3cb58e6ade3d9362fc563b483ad01c009
Gia Huy Le <giahuyle1030@gmail.com>
2025-04-01 13:59:40 +0700

    fix: TC14 & TC15

--- PersonalExpenseManagement/src/main/java/com/ghee/config/AppConfigs.java
+++ PersonalExpenseManagement/src/main/java/com/ghee/config/AppConfigs.java
@@ -9,15 +9,17 @@ package com.ghee.config;
  * @author giahu
  */
 public class AppConfigs {
-    public static int LENGHT_OF_ACCOUNT = 7;
     
     // ^ và $ đảm bảo kiểm tra toàn bộ chuỗi
     // .+ là kiểm tra không rỗng.
     public static String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*(),.?\":{}|<>]).+$";
+    public static int LENGHT_OF_ACCOUNT = 7;
+    
+    public static String ERROR_NOT_ENOUGH_INFORMATION = "Vui lòng điền đầy đủ thông tin !";
+    public static String ERROR_EMAIL_PATTERN = "Vui lòng điền email hợp lệ thiếu @ !";
     
     public static String ERROR_LENGHT_OF_ACCOUNT = "Độ dài không hợp lệ, vui lòng nhập lại username và password phải trên 7 ký tự !";
-    public static String ERROR_PASS_AND_CONFIRM = "Lỗi mật khẩu không trùng khớp, vui lòng nhập lại !";
     public static String ERROR_PASS_PATTERN = "Lỗi mật khẩu không đủ mạnh, phải có ít nhất 1 ký tự hoa, 1 ký tự thường, 1 số và 1 ký tự đặc biệt !";
-    public static String ERROR_NOT_ENOUGH_INFORMATION = "Vui lòng điền đầy đủ thông tin !";
-    public static String ERROR_EMAIL_PATTERN = "Vui lòng điền đầy đủ thông tin !";
+    public static String ERROR_PASS_AND_CONFIRM = "Lỗi mật khẩu không trùng khớp, vui lòng nhập lại !";
+    
 }
