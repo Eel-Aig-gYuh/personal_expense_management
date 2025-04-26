2e312f82099f92e874aa6d2e47c4d6a1da5b82bd
Gia Huy Le <giahuyle1030@gmail.com>
2025-04-26 10:43:11 +0700

    style: register ui

--- PersonalExpenseManagement/src/main/java/com/ghee/personalexpensemanagement/BudgetCreatePageController.java
+++ PersonalExpenseManagement/src/main/java/com/ghee/personalexpensemanagement/BudgetCreatePageController.java
@@ -202,11 +202,16 @@ public class BudgetCreatePageController implements Initializable {
                 // MessageBox.getAlert(AppConfigs.ERROR_TARGET_IS_NEGATIVE, Alert.AlertType.WARNING).showAndWait();
                 MessageErrorField.ErrorFieldHbox(getTxtTarget(), AppConfigs.ERROR_TARGET_IS_NEGATIVE);
                 return; 
-            } else if (target < 100000) {
+            } else if (target <= AppConfigs.MIN_TARGET) {
                 // MessageBox.getAlert(AppConfigs.ERROR_TARGET_LESS_THAN_MIN, Alert.AlertType.WARNING).showAndWait();
                 MessageErrorField.ErrorFieldHbox(getTxtTarget(), AppConfigs.ERROR_TARGET_LESS_THAN_MIN);
                 return;
-            } else {
+            } else if (target >= AppConfigs.MAX_TARGET) {
+                // MessageBox.getAlert(AppConfigs.ERROR_TARGET_LESS_THAN_MIN, Alert.AlertType.WARNING).showAndWait();
+                MessageErrorField.ErrorFieldHbox(getTxtTarget(), AppConfigs.ERROR_TARGET_LESS_THAN_MIN);
+                return; 
+            }
+            else {
                 MessageErrorField.ErrorFieldHboxOff(getTxtTarget());
             }
             
